2341e01114e71a4b8ed0febf8aa2b01966d3d9f1
Botnari Bogdan <141923746+Staycoo10@users.noreply.github.com>
2025-09-22 18:49:27 +0300

    implementing restriction admins can only change their own apartments

--- src/controllers/apartmentController.js
+++ src/controllers/apartmentController.js
@@ -29,7 +29,8 @@ const getApartmentById = async (req, res) => {
 // Create apartment
 const createApartment = async (req, res) => {
   try {
-    const { title, description, location, price, is_available, owner_id } = req.body;
+    const { title, description, location, price, is_available,} = req.body;
+    const owner_id = req.session.userId
     const result = await pool.query(
       `INSERT INTO apartments (title, description, location, price, is_available, owner_id)
        VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
@@ -47,14 +48,15 @@ const updateApartment = async (req, res) => {
   try {
     const { id } = req.params;
     const { title, description, location, price, is_available } = req.body;
+    const owner_id = req.session.userId;
     const result = await pool.query(
       `UPDATE apartments
        SET title=$1, description=$2, location=$3, price=$4, is_available=$5
-       WHERE id=$6 RETURNING *`,
-      [title, description, location, price, is_available, id]
+       WHERE id=$6 AND owner_id=$7 RETURNING *`,
+      [title, description, location, price, is_available, id, owner_id]
     );
     if (result.rows.length === 0) {
-      return res.status(404).json({ error: "Apartment not found" });
+      return res.status(403).json({ error: "You can edit only your own apartments" });
     }
     res.json(result.rows[0]);
   } catch (error) {
@@ -68,8 +70,9 @@ const deleteApartment = async (req, res) => {
   try {
     const { id } = req.params;
     const result = await pool.query("DELETE FROM apartments WHERE id=$1 RETURNING *", [id]);
+    const owner_id = req.session.userId;
     if (result.rows.length === 0) {
-      return res.status(404).json({ error: "Apartment not found" });
+      return res.status(403).json({ error: "You can delete only your own apartments" });
     }
     res.json({ message: "Apartment deleted" });
   } catch (error) {
