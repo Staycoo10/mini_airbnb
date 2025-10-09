7e386ab992e725cc879184ffc14ff7ebd0f58276
Botnari Bogdan <141923746+Staycoo10@users.noreply.github.com>
2025-10-09 10:40:27 +0300

    Add reservation routes to server
    Imported and registered the reservation routes in the Express server. Also added a new reservationController.js file for future reservation-related logic.

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const authRoutes = require("./routes/auth");
 const apartamentRoutes = require("./routes/apartments");
+const reservationRoutes = require("./routes/reservations");
 
 const app = express();
 
@@ -21,7 +22,7 @@ app.use(
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/apartments", apartamentRoutes);
-
+app.use("/api/reservations", reservationRoutes);
 // Home route
 app.get("/", (req, res) => {
   res.send("Mini-Airbnb backend is running!");
