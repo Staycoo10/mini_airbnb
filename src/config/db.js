3326c7294f6135a4365f1d301c09ae62472b5557
Botnari Bogdan <141923746+Staycoo10@users.noreply.github.com>
2025-11-24 09:43:11 +0200

    Use environment variables for DB config
    Replaced hardcoded PostgreSQL connection parameters with environment variables to improve security and flexibility.

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
 const {Pool} = require('pg');
 const pool = new Pool({ 
-    user: 'postgres',
-    host: 'localhost',
-    database: 'mini_airbnb',
-    password: 'admin',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 module.exports = pool;
